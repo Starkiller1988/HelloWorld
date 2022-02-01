443d1a98c8d458a9a5631df7e43f1ce9d12b5cc5
Starkiller1988 <schabel.samir@gmail.com>
2022-02-01 17:57:18 +0100

    added math

--- src/MathApp.java
+++ src/MathApp.java
@@ -5,5 +5,20 @@ public class MathApp {
         int math2 = 5;
         int resultMath = math1 + math2;
         System.out.println( "Summe "+ resultMath );
+
+        int numberOne = 29;
+        int numberTwo = 84;
+        boolean result = numberOne >= numberTwo;
+        System.out.println(result);
+
+        byte b1 = 23;
+        byte b2 = 54;
+        boolean resultB = b1 < b2;
+        System.out.println(resultB);
+
+        int isTrue = 0;
+        int isFalse = 1;
+        boolean resultTrueFalse = isTrue > isFalse;
+        System.out.println(resultTrueFalse);
     }
 }
