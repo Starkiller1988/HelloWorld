0a576a7eaa21f9955e52484dba53ceedaf661de3
Starkiller1988 <schabel.samir@gmail.com>
2022-02-01 15:44:35 +0100

    added math

---
+++ src/MathApp.java
@@ -0,0 +1,9 @@
+public class MathApp {
+
+    public static void main(String[] args){
+        int math1 = 2;
+        int math2 = 5;
+        int resultMath = math1 + math2;
+        System.out.println( "Summe "+ resultMath );
+    }
+}
